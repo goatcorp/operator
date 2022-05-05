34bd4d4d585364723ef88075e2a69d67a25beeaa
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-05-05 14:39:17 -0700

    Fix ID column nullability

--- sql/000-CreateReaderTable.sql
+++ sql/000-CreateReaderTable.sql
@@ -2,8 +2,8 @@ CREATE TABLE IF NOT EXISTS Reader (
     id              SERIAL,
     github          VARCHAR(40) UNIQUE,
     email           VARCHAR(40) UNIQUE NOT NULL,
-    report_interval INTERVAL NOT NULL,
-    active          BOOLEAN NOT NULL,
+    report_interval INTERVAL    NOT NULL,
+    active          BOOLEAN     NOT NULL,
 
     PRIMARY KEY (id)
 );
--- sql/001-CreateReportTable.sql
+++ sql/001-CreateReportTable.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS Report (
     id             SERIAL,
     sent_time      TIMESTAMP WITH TIME ZONE NOT NULL,
-    reader_id      INTEGER NOT NULL,
+    reader_id      INTEGER                  NOT NULL,
 
     PRIMARY KEY (id),
     FOREIGN KEY (reader_id) REFERENCES Reader(id) DEFERRABLE INITIALLY DEFERRED
