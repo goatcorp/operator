45517f82b1c3ce8540c9633271f9383a8add0004
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-05-05 10:04:22 -0700

    Set up email sending

--- go.mod
+++ go.mod
@@ -6,8 +6,11 @@ require github.com/reugn/go-quartz v0.3.9
 
 require (
 	github.com/jackc/pgx v3.6.2+incompatible // indirect
+	github.com/jordan-wright/email v4.0.1-0.20210109023952-943e75fe5223+incompatible // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	golang.org/x/text v0.3.3 // indirect
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
+	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df // indirect
 )
 
 require (
--- main.go
+++ main.go
@@ -2,22 +2,52 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"hash/fnv"
 	"log"
+	"net/smtp"
 	"os"
 	"time"
 
 	"github.com/google/go-github/v44/github"
 	"github.com/jackc/pgx"
+	"github.com/jordan-wright/email"
 	"github.com/reugn/go-quartz/quartz"
 )
+
+// https://gist.github.com/homme/22b457eb054a07e7b2fb
+type loginAuth struct {
+	username, password string
+}
+
+func LoginAuth(username, password string) smtp.Auth {
+	return &loginAuth{username, password}
+}
+
+func (a *loginAuth) Start(server *smtp.ServerInfo) (string, []byte, error) {
+	return "LOGIN", []byte(a.username), nil
+}
+
+func (a *loginAuth) Next(fromServer []byte, more bool) ([]byte, error) {
+	if more {
+		switch string(fromServer) {
+		case "Username:":
+			return []byte(a.username), nil
+		case "Password:":
+			return []byte(a.password), nil
+		default:
+			return nil, errors.New("unknown fromServer")
+		}
+	}
+	return nil, nil
+}
 
 type ReportJob struct{}
 
 func (j *ReportJob) Execute() {
 	client := github.NewClient(nil)
-	repos, _, err := client.PullRequests.List(context.Background(), "goatcorp", "DalamudPlugins", &github.PullRequestListOptions{
+	plogons, _, err := client.PullRequests.List(context.Background(), "goatcorp", "DalamudPlugins", &github.PullRequestListOptions{
 		State: "open",
 	})
 
@@ -26,9 +56,9 @@ func (j *ReportJob) Execute() {
 		return
 	}
 
-	for _, repo := range repos {
-		if repo.Title != nil {
-			log.Println(*repo.Title)
+	for _, plogon := range plogons {
+		if plogon.Title != nil {
+			log.Println(*plogon.Title)
 		} else {
 			log.Println()
 		}
@@ -44,6 +74,21 @@ func (j *ReportJob) Execute() {
 		os.Exit(1)
 	}
 	defer conn.Close()
+
+	a := LoginAuth(os.Getenv("OPERATOR_EMAIL"), os.Getenv("OPERATOR_PASSWORD"))
+	from := fmt.Sprintf("Caprine Operator <%s>", os.Getenv("OPERATOR_EMAIL"))
+	to := []string{""}
+	msg := []byte("test")
+
+	e := email.NewEmail()
+	e.From = from
+	e.To = to
+	e.Text = msg
+	err = e.Send(os.Getenv("OPERATOR_SMTP_SERVER"), a)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to send mail: %v\n", err)
+		os.Exit(1)
+	}
 }
 
 func (j *ReportJob) Description() string {
